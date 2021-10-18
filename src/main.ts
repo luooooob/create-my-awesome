104eb59a961a1ba551a2481122b341900d4fd066
Jiang Yan <contact@jiang-yan.com>
2021-10-18 19:04:32 +0800

    Remove catch error

--- src/main.ts
+++ src/main.ts
@@ -12,29 +12,26 @@ import type { Repo } from './types'
 dotenv.config()
 
 const main = async () => {
-  try {
-    const username = getInput(`username`)
-    const repository = getInput(`repository`)
-    const description = getInput(`description`)
-    const workflow = getInput(`workflow`)
-    const token = getInput(`token`)
-    const targetDir = getInput(`targetDir`)
 
-    const github = getOctokit(token)
-    const collection = Array<Repo>()
-    await fetchRepos(github, collection, username)
+  const username = getInput(`username`)
+  const repository = getInput(`repository`)
+  const description = getInput(`description`)
+  const workflow = getInput(`workflow`)
+  const token = getInput(`token`)
+  const targetDir = getInput(`targetDir`)
 
-    await mkdirp(targetDir)
+  const github = getOctokit(token)
+  const collection = Array<Repo>()
+  await fetchRepos(github, collection, username)
 
-    const md = renderToMd(repository, description, workflow, collection)
-    const mdFilename = join(targetDir, 'README.md')
-    info(`write file: "${mdFilename}"`)
-    info(md)
-    await fs.writeFile(mdFilename, md)
+  await mkdirp(targetDir)
 
-  } catch (err) {
-    error(err)
-  }
+  const md = renderToMd(repository, description, workflow, collection)
+  const mdFilename = join(targetDir, 'README.md')
+  info(`write file: "${mdFilename}"`)
+  info(md)
+  await fs.writeFile(mdFilename, md)
+
 }
 
 main()
